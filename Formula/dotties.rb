e2103febe950c58da865d90e8b52b0d410dbe824
Blaine Schmeisser <blainesch@gmail.com>
2016-08-29 20:12:36 -0700

    Update formula for dotties to v0.4.0.

--- Formula/dotties.rb
+++ Formula/dotties.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Dotties < Formula
   homepage 'https://github.com/tinytacoteam/dotties-core'
-  url 'https://github.com/tinytacoteam/dotties-core/archive/v0.3.1.tar.gz'
-  sha256 'db73338c5c91e68be864193e10c22a0e277380d8c5b60c77f3a985d2459bc695'
+  url 'https://github.com/tinytacoteam/dotties-core/archive/v0.4.0.tar.gz'
+  sha256 '4605968570aa77a240eb12bbcf5e9fd775021f3de1e365d7e8fb7563e649a519'
 
   def install
     libexec.install Dir["*"] << '.version'
