87f14ade09593ef9c9bb1e53081c0f5bcdf6751a
Blaine Schmeisser <blainesch@gmail.com>
2016-08-29 20:15:46 -0700

    Update git sha for v0.4.0

--- Formula/dotties.rb
+++ Formula/dotties.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Dotties < Formula
   homepage 'https://github.com/tinytacoteam/dotties-core'
   url 'https://github.com/tinytacoteam/dotties-core/archive/v0.4.0.tar.gz'
-  sha256 '4605968570aa77a240eb12bbcf5e9fd775021f3de1e365d7e8fb7563e649a519'
+  sha256 'e1a8f543d362a03d522564dbe7b9a428b1fab32c37c040274b512db3c1c0b72f'
 
   def install
     libexec.install Dir["*"] << '.version'
